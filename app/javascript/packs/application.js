797bfc1ca07bf2720082f55972538123b94698c0
yuyakinoshita <yuya92220087@gmail.com>
2020-10-26 12:19:20 +0900

    fontawesomeを導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("javascript/top")
 
 import 'bootstrap';
 import '../stylesheets/application';
+import '@fortawesome/fontawesome-free/js/all';
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
